2933a83a6b681abaca00e51bb9e79ffe7f9f4fd3
André Assis <andreassis240256@gmail.com>
2025-12-10 16:08:43 -0300

    Add materials properties with light

--- src/shaders/fragment_shader.glsl
+++ src/shaders/fragment_shader.glsl
@@ -1,33 +1,48 @@
 #version 330 core
+
+struct Light{
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+struct Material{
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+uniform Light light;
+uniform Material material;
+
 out vec4 FragColor;
 
 in vec3 normal;
 in vec3 FragPos;
 
 uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPos;
 uniform vec3 viewPos;
 
 void main()
 {
     //AMBIENT LIGHT
-    float ambientStrength = 0.1f;
-    vec3 ambient = ambientStrength * lightColor;
+    vec3 ambient = material.ambient * light.ambient;
 
     //DIFUSE LIGHT
     vec3 norm = normalize(normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0f);
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = diff * material.diffuse * light.diffuse;
 
     //SPECULAR LIGHT
-    float specularStrenght = 0.5f;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrenght * spec * lightColor;
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = material.specular * spec * light.specular;
 
-    vec3 result = (ambient + diffuse + specular) * objectColor;
+    vec3 result = (ambient + diffuse + specular);
     FragColor = vec4(result, 1.0);
 }
