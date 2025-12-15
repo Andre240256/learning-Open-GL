b506548431c3abb6b2825710b63e43928333c177
André Assis <andreassis240256@gmail.com>
2025-12-15 15:20:10 -0300

    Reorganized shaders files from chapter 17

--- src/lighting/chapter17/fragment_shader_chapter17.glsl
+++ src/lighting/chapter17/fragment_shader_chapter17.glsl
@@ -1,8 +1,11 @@
 #version 330 core
 
 struct Material{
-    sampler2D diffuse;
-    sampler2D specular;
+    uniform sampler2D texture_diffuse1;
+    uniform sampler2D texture_diffuse2;
+    uniform sampler2D texture_diffuse3;
+    uniform sampler2D texture_specular1;
+    uniform sampler2D texture_specular2;
 
     float shininess;
 };
