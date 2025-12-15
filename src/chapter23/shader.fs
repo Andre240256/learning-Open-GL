9b3ce286bb6201419f152b71620051c14af16d3b
André Assis <andreassis240256@gmail.com>
2025-12-15 13:27:01 -0300

    Add chapter 23

---
+++ src/chapter23/shader.fs
@@ -0,0 +1,22 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D texture1;
+
+float near = 0.1;
+float far = 100.0;
+
+float LinearizeDepth(float depth)
+{
+    float z = depth * 2.0 - 1.0f;
+
+    return (2.0f * near * far) / (far + near - z * (far - near));
+}
+
+void main()
+{    
+    
+    FragColor = texture(texture1, TexCoords);
+}
